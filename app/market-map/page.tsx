f4d3b138161aef4386c6026b62030d22db262396
Crystal Tai <crystaltai.xyz@gmail.com>
2025-05-13 14:10:01 -0700

    market map placeholder page

---
+++ app/market-map/page.tsx
@@ -0,0 +1,9 @@
+const MarketMap = () => {
+  return (
+    <div className="w-full max-w-screen-xl mx-auto p-8 pb-12 flex flex-col gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      [ Market Map placeholder ]
+    </div>
+  )
+}
+
+export default MarketMap
