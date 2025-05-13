e3db472a301eea0a4b9e389611951082f4557ea8
Crystal Tai <crystaltai.xyz@gmail.com>
2025-05-13 14:26:17 -0700

    content wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import {
 import Chart from '@/components/chart'
 import StatSummaryTile from '@/components/stat-summary-tile'
 import Blurb from '@/components/blurb'
+import ContentWrapper from '@/components/(layout)/content-wrapper'
 
 export default function Overview() {
   const dummyStats = [
@@ -18,71 +19,80 @@ export default function Overview() {
   ]
 
   return (
-    <div className="w-full max-w-screen-xl mx-auto p-8 pb-12 flex flex-col gap-32 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="w-full p-8 pb-12 flex flex-col gap-32 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <StatSummaryTile miniStatsData={dummyStats} />
-      <div className="flex flex-col gap-8 items-center">
-        <Blurb
-          title="How Big are Crypto Payments, Really?"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
-          textAlignment="center"
-        />
-        <Chart
-          title="Payment Usage by Type"
-          data={PAYMENT_USAGE_BY_TYPE_DATA}
-          dataConfig={PAYMENT_USAGE_BY_TYPE_CONFIG}
-          valueFormat={VALUE_FORMAT.currency}
-          isTimeSeries
-          chartHeight={375}
-        />
-      </div>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
-        <div className="col-span-2">
+      <ContentWrapper>
+        <div className="flex flex-col gap-8 items-center">
+          <Blurb
+            title="How Big are Crypto Payments, Really?"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "
+            textAlignment="center"
+          />
           <Chart
-            title="Stablecoin vs Crypto Performance (Index to 100)"
-            data={AVG_RETENTION_RATE_DATA}
-            dataConfig={AVG_RETENTION_RATE_CONFIG}
-            valueFormat={VALUE_FORMAT.percentage}
-            xAxisLabel="Months"
+            title="Payment Usage by Type"
+            data={PAYMENT_USAGE_BY_TYPE_DATA}
+            dataConfig={PAYMENT_USAGE_BY_TYPE_CONFIG}
+            valueFormat={VALUE_FORMAT.currency}
+            isTimeSeries
+            chartHeight={375}
           />
         </div>
-        <Blurb
-          title="Stablecoin Market Cap Trends Upwards While Prices Rebound"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-      </div>
+      </ContentWrapper>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
-        <Blurb
-          title="Stablecoins are Attractive Globally"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-        <div className="col-span-2">
-          <Chart
-            title="Adjusted Number of Transactions by Region"
-            data={AVG_RETENTION_RATE_DATA}
-            dataConfig={AVG_RETENTION_RATE_CONFIG}
-            valueFormat={VALUE_FORMAT.percentage}
-            xAxisLabel="Months"
+      <ContentWrapper>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+          <div className="col-span-2">
+            <Chart
+              title="Stablecoin vs Crypto Performance (Index to 100)"
+              data={AVG_RETENTION_RATE_DATA}
+              dataConfig={AVG_RETENTION_RATE_CONFIG}
+              valueFormat={VALUE_FORMAT.percentage}
+              xAxisLabel="Months"
+            />
+          </div>
+          <Blurb
+            title="Stablecoin Market Cap Trends Upwards While Prices Rebound"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
         </div>
-      </div>
+      </ContentWrapper>
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
-        <div className="col-span-2">
-          <Chart
-            title="Avg Retention Rate Across Crypto Categories"
-            data={AVG_RETENTION_RATE_DATA}
-            dataConfig={AVG_RETENTION_RATE_CONFIG}
-            valueFormat={VALUE_FORMAT.percentage}
-            xAxisLabel="Months"
+      <ContentWrapper>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+          <Blurb
+            title="Stablecoins are Attractive Globally"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+          />
+          <div className="col-span-2">
+            <Chart
+              title="Adjusted Number of Transactions by Region"
+              data={AVG_RETENTION_RATE_DATA}
+              dataConfig={AVG_RETENTION_RATE_CONFIG}
+              valueFormat={VALUE_FORMAT.percentage}
+              xAxisLabel="Months"
+            />
+          </div>
+        </div>
+      </ContentWrapper>
+
+      <ContentWrapper>
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-16">
+          <div className="col-span-2">
+            <Chart
+              title="Avg Retention Rate Across Crypto Categories"
+              data={AVG_RETENTION_RATE_DATA}
+              dataConfig={AVG_RETENTION_RATE_CONFIG}
+              valueFormat={VALUE_FORMAT.percentage}
+              xAxisLabel="Months"
+            />
+          </div>
+          <Blurb
+            title="High Retention, Low Volatility: Why Stablecoins Are Built to Stay"
+            description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
           />
         </div>
-        <Blurb
-          title="High Retention, Low Volatility: Why Stablecoins Are Built to Stay"
-          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-        />
-      </div>
+      </ContentWrapper>
     </div>
   )
 }
---
+++ components/(layout)/content-wrapper.tsx
@@ -0,0 +1,5 @@
+const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
+  return <div className="w-full max-w-screen-xl">{children}</div>
+}
+
+export default ContentWrapper
