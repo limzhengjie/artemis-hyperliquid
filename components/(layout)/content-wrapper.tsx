afd7409e811b748233849200f0754fa12fef27db
Crystal Tai <crystaltai.xyz@gmail.com>
2025-05-21 19:20:19 -0400

    add id prop to content wrapper

--- components/(layout)/content-wrapper.tsx
+++ components/(layout)/content-wrapper.tsx
@@ -2,13 +2,20 @@
 
 import { motion } from 'framer-motion'
 
-const ContentWrapper = ({ children }: { children: React.ReactNode }) => {
+const ContentWrapper = ({
+  children,
+  id
+}: {
+  children: React.ReactNode
+  id?: string
+}) => {
   return (
     <motion.div
       className="w-full max-w-6xl px-4 md:px-8"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.5 }}
+      id={id}
     >
       {children}
     </motion.div>
