4d31caddc47e81a22173479be14ffb6b3d1d0bd4
Crystal Tai <crystaltai.xyz@gmail.com>
2025-05-21 21:46:51 -0400

    remove feedback button on footer

--- components/(layout)/footer.tsx
+++ components/(layout)/footer.tsx
@@ -1,20 +1,11 @@
 'use client'
 
-import { Button } from '@/components/ui/button'
 import ArtemisLogo from '@/components/(layout)/artemis-logo'
-
-import { MessageCircleMoreIcon } from 'lucide-react'
 
 const Footer = () => {
   return (
     <footer className="flex items-center justify-between px-6 py-2 border-t border-[var(--color-moongray-25)]">
       <ArtemisLogo poweredBy />
-      <div className="flex items-center gap-2">
-        <Button variant="outline" className="flex items-center gap-1">
-          <MessageCircleMoreIcon />
-          <span>Give us feedback</span>
-        </Button>
-      </div>
     </footer>
   )
 }
