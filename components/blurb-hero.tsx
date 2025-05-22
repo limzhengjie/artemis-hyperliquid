5c23f0ee1451d0f7a0506b48f78dd7d4d1b27088
Crystal Tai <crystaltai.xyz@gmail.com>
2025-05-22 15:31:14 -0400

    hero content

--- components/blurb-hero.tsx
+++ components/blurb-hero.tsx
@@ -6,33 +6,35 @@ import DragonflyLogo from '@/public/partners/dragonfly.png'
 const BlurbHero = () => {
   return (
     <div className="w-full flex flex-col gap-4 max-w-4xl">
-      <h1 className="text-foreground font-semibold text-[32px] md:text-[54px] leading-[32px] md:leading-[54px] text-left">
+      <h1 className="text-foreground font-semibold text-[32px] md:text-[48px] leading-[32px] md:leading-[48px] text-left">
         Stablecoin Payments from the Ground Up
       </h1>
       <div className="flex flex-col gap-4">
         <p className="text-muted-foreground text-md">In partnership with</p>
         <div className="flex gap-8 items-center">
           <div>
-            <Image src={CIVLogo} alt="CIV Logo" width={200} height={200} />
+            <Image src={CIVLogo} alt="CIV Logo" width={180} height={200} />
           </div>
           <div>
             <Image
               src={DragonflyLogo}
               alt="Dragonfly Logo"
-              width={200}
+              width={180}
               height={200}
             />
           </div>
         </div>
       </div>
       <p className="text-muted-foreground text-md">
-        Stablecoins have emerged as a global alternative to traditional payment
-        and settlement networks, with total supply growing from under $10
-        billion five years ago to approximately $240 billion today. In this
-        study, we present a novel dataset compiled from 38 stablecoin-based
-        payment firms that process transactions on behalf of end users. This is
-        the most comprehensive effort to date and likely captures the majority
-        of activity in the emerging stablecoin payments sector.
+        Stablecoins have quickly become a credible alternative to traditional
+        payment and settlement networks. In just five years, their supply has
+        grown from under $10 billion to over $240 billion. To better understand
+        this how stablecoins are actually used, we spoke with 20 companies
+        across a range of use cases and collected estimates from 11 more.
+        Altogether, they’ve settled $94.2 billion in payments since 2020, with
+        current volumes pointing to a $72.3 billion annual run rate. This report
+        offers a rare, bottom-up look at how stablecoins are being used in the
+        real world.
       </p>
     </div>
   )
