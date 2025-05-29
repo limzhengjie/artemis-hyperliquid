f39f1bdf7e3d75bf2805240043ccf445027bcf39
Crystal Tai <crystaltai.xyz@gmail.com>
2025-05-29 09:59:01 -0400

    footer bigger text

--- components/(layout)/footer.tsx
+++ components/(layout)/footer.tsx
@@ -7,7 +7,7 @@ const Footer = () => {
   return (
     <footer className="flex items-center justify-between px-6 py-2 border-t border-[var(--color-moongray-25)]">
       <ArtemisLogo poweredBy />
-      <div className="hidden md:flex items-center gap-2 text-sm text-muted-foreground">
+      <div className="hidden md:flex items-center gap-2 text-md text-muted-foreground">
         <Link href="mailto:press@artemisanalytics.com">Press Contact</Link>
         <p>•</p>
         <Link href="/methodology">Methodology</Link>
