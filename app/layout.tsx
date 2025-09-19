1275232f5d7f81122b27742b7a10546e543265e4
Crystal Tai <crystaltai.xyz@gmail.com>
2025-09-18 20:08:11 -0400

    force-dynamic

--- app/layout.tsx
+++ app/layout.tsx
@@ -59,6 +59,9 @@ export const metadata: Metadata = {
   }
 }
 
+export const revalidate = 0
+export const dynamic = 'force-dynamic'
+
 export default function RootLayout({
   children
 }: Readonly<{
